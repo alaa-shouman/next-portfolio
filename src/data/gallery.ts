537d8947ac43f1e5f12bc0d11e7eba01525089b3
alaa <alaa.shouman@st.edu.ul.lb>
2025-09-30 10:33:37 +0300

    Refactor code structure for improved readability and maintainability

--- src/data/gallery.ts
+++ src/data/gallery.ts
@@ -2,6 +2,12 @@ import Img1 from "/public/assets/images/gallery/Alaa.png";
 import Img2 from "/public/assets/images/gallery/Alaa_2.jpg";
 import Img3 from "/public/assets/images/gallery/Alaa_3.jpg";
 import Img4 from "/public/assets/images/gallery/Alaa_4.jpg";
+import Img5 from "/public/assets/images/gallery/Alaa_4.jpg";
+import Img6 from "/public/assets/images/gallery/Alaa_5.jpg";
+import Img7 from "/public/assets/images/gallery/Alaa_6.jpg";
+import Img8 from "/public/assets/images/gallery/Alaa_7.jpg";
+import Img9 from "/public/assets/images/gallery/Alaa_8.jpg";
+import Img10 from "/public/assets/images/gallery/Alaa_9.jpg";
 
 export const galleryImages = [
   {
@@ -19,6 +25,30 @@ export const galleryImages = [
   {
     id: 3,
     img: Img4,
+  },
+  {
+    id: 4,
+    img: Img5,
+  },
+  {
+    id: 5,
+    img: Img6,
+  },
+  {
+    id: 6,
+    img: Img7,
+  },
+  {
+    id: 7,
+    img: Img8,
+  },
+  {
+    id: 8,
+    img: Img9,
+  },
+  {
+    id: 9,
+    img: Img10,
   },
 ];
 
