ca1daa64689f958da6ec11b6e2fe76c19203d2c3
alaa <alaa.shouman@st.edu.ul.lb>
2026-02-08 18:01:12 +0200

    me fixes

--- src/components/card/Me.tsx
+++ src/components/card/Me.tsx
@@ -6,43 +6,43 @@ import Card from "../UI/Card";
 export default function MeCard() {
     return (
         <Card className="2xl:h-full">
-            <div className="w-full h-[25rem] sm:h-[37.5rem] overflow-hidden">
+            <div className="w-full h-[28rem] sm:h-[37.5rem] relative overflow-hidden">
                 {/*background image*/}
                 <Image
                     src={MyImg}
                     alt="Alaa Shouman"
                     className="absolute top-0 left-0 bottom-0 right-0 h-full w-full object-cover"
                 />
-            </div>
-            <div className="absolute top-[65%] space-y-2">
-                <div className="bg-black/[0.7] w-fit py-1.5 px-3 rounded-full">
-                    <p className="text-primary-foreground leading-[110%] font-bold">
-                        Hello, universe 👋
-                    </p>
-                </div>
+                <div className="absolute top-[50%] sm:top-[60%] left-0 right-0 px-4 space-y-1.5 sm:space-y-2 pb-4">
+                    <div className="bg-black/[0.7] w-fit py-1.5 px-3 rounded-full">
+                        <p className="text-primary-foreground leading-[110%] font-bold">
+                            Hello, universe 👋
+                        </p>
+                    </div>
 
-                <div className="bg-black/[0.7] w-fit py-1.5 px-3 rounded-full">
-                    <p className="text-white text-sm font-medium">
-                        Full Stack Developer 💻
-                    </p>
-                </div>
+                    <div className="bg-black/[0.7] w-fit py-1.5 px-3 rounded-full">
+                        <p className="text-white text-sm font-medium">
+                            Full Stack Developer 💻
+                        </p>
+                    </div>
 
-                <div className="bg-black/[0.7] w-fit py-1.5 px-3 rounded-full">
-                    <p className="text-white text-sm font-medium">
-                        React Native Expert 📱
-                    </p>
-                </div>
+                    <div className="bg-black/[0.7] w-fit py-1.5 px-3 rounded-full">
+                        <p className="text-white text-sm font-medium">
+                            React Native Expert 📱
+                        </p>
+                    </div>
 
-                <div className="bg-black/[0.7] w-fit py-1.5 px-3 rounded-full">
-                    <p className="text-white text-sm font-medium">
-                        Problem Solver 🚀
-                    </p>
-                </div>
+                    <div className="bg-black/[0.7] w-fit py-1.5 px-3 rounded-full">
+                        <p className="text-white text-sm font-medium">
+                            Problem Solver 🚀
+                        </p>
+                    </div>
 
-                <div className="bg-black/[0.7] w-fit py-1.5 px-3 rounded-full">
-                    <p className="text-white text-sm font-medium">
-                        Tea Enthusiast ☕
-                    </p>
+                    <div className="bg-black/[0.7] w-fit py-1.5 px-3 rounded-full">
+                        <p className="text-white text-sm font-medium">
+                            Tea Enthusiast ☕
+                        </p>
+                    </div>
                 </div>
             </div>
         </Card>
